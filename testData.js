aa53f003e3b8e3b7ae44e269f0dac981d421814e
Denny Hollick <dennyhollick@gmail.com>
2017-09-15 11:51:47 -0700

    Cleaned test data

--- testData.js
+++ testData.js
@@ -1,63 +1,63 @@
 const testData = [
   {
     data: [],
-    expected: 'Should return err',
+    expected: 'err',
   },
   {
     data: [1],
-    expected: 'Should return 1',
+    expected: 1,
   },
   {
     data: [-1],
-    expected: 'Should return 1',
+    expected: 1,
   },
   {
     data: [0],
-    expected: 'Should return 1',
+    expected: 1,
   },
   {
     data: ['a'],
-    expected: 'Should return err',
+    expected: 'err',
   },
   {
     data: [1, 2],
-    expected: 'Should return 2',
+    expected: 2,
   },
   {
     data: [1, 1],
-    expected: 'Should return 1',
+    expected: 1,
   },
   {
     data: [1, 2, 2],
-    expected: 'Should return 2',
+    expected: 2,
   },
   {
     data: [2, 2, 2],
-    expected: 'Should return 1',
+    expected: 1,
   },
   {
     data: [1, 2, 1],
-    expected: 'Should return 3',
+    expected: 3,
   },
   {
     data: [1, 2, 1, 2],
-    expected: 'Should return 4',
+    expected: 4,
   },
   {
     data: [1, 1, 1, 2, 2, 2, 1, 1, 1],
-    expected: 'Should return 3',
+    expected: 3,
   },
   {
     data: [1, 2, 'a', 3],
-    expected: 'Should return err',
+    expected: 'err',
   },
   {
     data: [-1, -2, -3, -2, -1],
-    expected: 'Should return 3',
+    expected: 3,
   },
   {
     data: [0, 1, 2, 1, 0, -1, 0, 1, 2, 3, 4, 5, 5, 5, 4, 3, 3, 4],
-    expected: 'Should return 6',
+    expected: 6,
   },
 ];
 
